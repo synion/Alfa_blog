2fb45e8ecba7551496668606665351d84715649d
synion <lach.szymon86@gmail.com>
2017-06-15 10:02:53 +0200

    Create,Update,Index for Article

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,46 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Artcile was sucesfull created"
+      redirect_to @article
+    else
+      flash[:errors] = @article.errors.full_messages
+      render 'new'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update
+      flash[:notice] = "Article was succesful update"
+      redirect_to @article
+    else
+      flash[:errors] = @article.errors.full_messages
+      render 'edit'
+    end
+  end
+
+
+  private
+    def article_params
+      params.require(:article).permit(:title,:description)
+    end
+end
