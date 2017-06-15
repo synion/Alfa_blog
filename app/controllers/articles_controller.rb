c0a6d9833b21dd8e15e230a1dd0eb3884bf4f56d
synion <lach.szymon86@gmail.com>
2017-06-15 15:32:34 +0200

    Prepera for production, remove repeter for article controler

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   def index
     @articles = Article.all
   end
@@ -20,15 +20,12 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = ["Article was succesful update"]
       redirect_to @article
@@ -39,7 +36,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = ["You delete article"]
     redirect_to articles_path
@@ -47,6 +43,11 @@ class ArticlesController < ApplicationController
 
 
   private
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title,:description)
     end
