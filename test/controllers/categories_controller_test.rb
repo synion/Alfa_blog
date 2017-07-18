a39484e844ce3ab13d58f7ee31db79815fca263a
synion <lach.szymon86@gmail.com>
2017-07-18 14:28:02 +0200

    Add test and action create with curate_user.admin

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class CategoriesControllerTest < ActionController::TestCase
   def setup
     @category = Category.create(name: "sports")
+    @user = User.create(name: "Szymon", email: "examle@o2.pl", password: "password", admin: true)
   end
 
   test "should get index action" do
@@ -11,6 +12,7 @@ class CategoriesControllerTest < ActionController::TestCase
   end
 
   test "should get new action" do
+    session[:user_id] = @user.id
     get :new
     assert_response :success
   end
@@ -21,7 +23,15 @@ class CategoriesControllerTest < ActionController::TestCase
   end
 
   test "should get edit action" do
+    session[:user_id] = @user.id
      get :edit, params: { id: @category.id }
     assert_response :success
+  end
+
+  test "should not create Categories if admin not login" do
+    assert_no_difference "Category.count" do
+      post :create, params: { name: "Work" }
+    end
+    assert_redirected_to categories_path
   end
 end
